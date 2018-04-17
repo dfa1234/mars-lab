9a5f5998672e94d5aa40aac4db3c80f53ef5034e
Thierry Chatel <tchatel@gmail.com>
2018-04-17 18:15:37 +0200

    @angular/cli v6.0.0-rc.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import {RoversModule} from './rovers/rovers.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { HomePageComponent } from './home-page/home-page.component';
   ],
   imports: [
     BrowserModule,
+    RoversModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/rovers/page-rovers-list/page-rovers-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-rovers-list',
+  templateUrl: './page-rovers-list.component.html',
+  styleUrls: ['./page-rovers-list.component.css']
+})
+export class PageRoversListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
