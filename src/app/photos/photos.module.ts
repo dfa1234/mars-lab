f118486321e421a22c544cec7f8ad8b0b455e814
Thierry Chatel <tchatel@gmail.com>
2018-04-17 19:46:57 +0200

    @angular/cli v6.0.0-rc.5

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PhotosRoutingModule } from './photos-routing.module';
 import { PagePhotosListComponent } from './page-photos-list/page-photos-list.component';
+import { PhotoItemComponent } from './photo-item/photo-item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PhotosRoutingModule
   ],
-  declarations: [PagePhotosListComponent]
+  declarations: [PagePhotosListComponent, PhotoItemComponent]
 })
 export class PhotosModule { }
